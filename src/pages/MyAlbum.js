8a89269f842c4379e7e6e80bb6169adcc36f63ac
이보현 <84188904+250b@users.noreply.github.com>
2023-04-29 23:57:19 +0900

    [MyAlbum] 마이 앨범 페이지 구현

--- src/pages/MyAlbum.js
+++ src/pages/MyAlbum.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import CustomButton from "../components/CustomButton"
 import { useNavigate } from "react-router-dom";
+import polaroid from '../images/polaroid.svg';
 
 function MyAlbum() {
   let navigate = useNavigate();
@@ -8,6 +9,13 @@ function MyAlbum() {
   return (
     <Container>
         <Title>MY ALBUM</Title>
+        <AlbumContainer>
+            <Album><Polar><img src={polaroid}/><span>first</span></Polar></Album>
+            <Album><Polar><img src={polaroid}/><span>first</span></Polar></Album>
+            <Album><Polar><img src={polaroid}/><span>first</span></Polar></Album>
+            <Album><Polar><img src={polaroid}/><span>first</span></Polar></Album>
+            <Album><Polar><img src={polaroid}/><span>first</span></Polar></Album>
+        </AlbumContainer>
     </Container>
   );
 }
@@ -21,9 +29,60 @@ const Container = styled.div`
 `
 
 const Title = styled.div`
-  font-size:60px;
-  @media Screen and (max-width:800px){
-    font-size:40px;
+  font-size:40px;
+  margin-top:30px;
+  margin-bottom:30px;
+  @media Screen and (max-width:600px){
+    font-size:30px;
+}
+
+`
+const AlbumContainer = styled.div`
+    display:grid;
+    grid-template-columns:repeat(3, 1fr);
+    margin:auto;
+    @media Screen and (max-width:900px){
+        grid-template-columns:repeat(2, 1fr);
+    }
+`
+const Album = styled.div`
+
+`
+const Polar = styled.div`
+  width:300px;
+  height:300px;
+  position:relative;
+  @media Screen and (max-width:600px){
+    width:180px;
+    height:180px;
+}
+  >img{
+    width:250px;
+    height:250px;
+    position:absolute;
+    left:25px;
+    @media Screen and (max-width:900px){
+        left:25px;
+    }
+    @media Screen and (max-width:600px){
+        width:170px;
+        height:170px; 
+        left:5px;
+    }
+  }
+  >span{
+    position:absolute;
+    bottom:65px;
+    font-size:20px;
+    left:115px;
+    @media Screen and (max-width:900px){
+        left:115px;
+    }
+    @media Screen and (max-width:600px){
+        font-size:15px;
+        bottom:20px;
+        left:65px;
+    }
   }
 `
 export default MyAlbum;
