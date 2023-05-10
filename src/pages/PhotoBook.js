64eae958e82b6a2b2d52e6b0e1274d5dc931e80e
이보현 <84188904+250b@users.noreply.github.com>
2023-05-10 17:17:20 +0900

    [PhotoBook] 코드 정돈 및 주석 추가

--- src/pages/PhotoBook.js
+++ src/pages/PhotoBook.js
@@ -1,119 +1,117 @@
 import styled from "styled-components";
 import React from "react";
 import Menu from "./Menu";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Edit from "./Edit";
-import frame2 from '../images/frame2.svg'
-import frame3 from '../images/frame3.svg'
 import Header from "../components/Header";
 import HTMLFlipBook from "react-pageflip";
-import { onAuthStateChanged, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import {getStorage, ref, uploadBytes, listAll, getDownloadURL,} from "firebase/storage";
-import { firestore ,firebaseApp,
-  firebaseDb,
-  firebaseStorage,} from '../firebase';
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { firestore } from '../firebase';
 
 function PhotoBook() {
+  let navigate = useNavigate();
     const location = useLocation();
-    const albumName = location.state.albumName.albumName;
-    console.log(location.state.albumName.albumName);
+    const albumName = location.state.albumName.albumName; //myalbum에서 넘겨준 state(앨범 이름)
     const [showMenu, setShowMenu] = useState(false);
     const [showEdit, setShowEdit] = useState(false);
 
+    //테마 정보
     const themeList={
       nature:["url('./nature1.jpeg')", "url('./nature2.jpeg')", "url('./nature3.jpeg')", "url('./nature4.jpeg')"],
        cat:["url('./cat1.jpeg')", "url('./cat2.jpeg')", "url('./cat3.jpeg')"],
        pattern:["url('./pattern1.jpeg')", "url('./pattern2.jpeg')", "url('./pattern3.png')", "url('./pattern4.jpeg')"],
        black:["url('./black1.jpeg')", "url('./black2.jpeg')", "url('./black3.jpeg')", "url('./black4.jpeg')"],
-       kitsch:["url('./kitsch1.jpeg')", "url('./kitsch2.jpeg')", "url('./kitsch3.jpeg')", "url('./kitsch4.jpeg')"],
+       kitsch:["url('./kitsch1.jpeg')", "url('./kitsch2.png')", "url('./kitsch3.jpeg')", "url('./kitsch4.jpeg')"],
   }
 
-
-    //유저 정보 확인해서 동일하고 이름도 동일한 앨범 가져오기
-    //앨범의 테마 가져와서 설정
     const auth = getAuth();
     let userEmail =""
     let themeName=""
+    const [forPropsThemeName, setForPropsThemeName] =useState("")
     const [theme, setTheme] =useState([])
     const [selectedImages, setSelectedImages] =useState([])
+    const [selectedTheme,setSelectedTheme] = useState("")
+
+
+  // 앨범 정보 가져오기
     onAuthStateChanged(auth, (user) => {
       if (user) {
         userEmail = user.email
-        console.log(userEmail)
+    const album = firestore.collection("album");
+    album.doc(albumName+userEmail).get().then((document)=>{ //state로 받은 앨범이름으로 앨범 정보 가져오기
+      const albumInfo = document.data();
+        themeName=albumInfo.theme
+        setTheme(themeList[themeName])
+        setSelectedTheme(themeName)
+        setForPropsThemeName(albumInfo.theme)
+        setSelectedImages(albumInfo.image)
+        })
       } else {
       }
     });
-    console.log(userEmail)
-    const album = firestore.collection("album");
-    album.doc(albumName).get().then((document)=>{
-      const albumInfo = document.data();
-      if(albumInfo.creater=userEmail){
-        themeName=albumInfo.theme
-        console.log(themeName)
-        console.log(themeList[themeName])
-        setTheme(themeList[themeName])
-        setSelectedImages(albumInfo.image)
 
-      }
-})
-// setTheme(themeList[themeName])
-useEffect(()=>{
-  // setTheme(themeList[themeName])
-  console.log(themeList[themeName])
-},[themeName])
-
-console.log(theme)
-    // const selectedImages = [frame2, frame3, frame2, frame3 ];
-
+  //메뉴 열기
     const toshowMenu = ()=>{
         setShowMenu(true)
     }
+
+  //메뉴 닫기
     const tocloseMenu = ()=>{
       setShowMenu(false)
   }
 
+  //앨범 수정 열기
   const toshowEdit=()=>{
     setShowEdit(true);
   }
-  const tocloseEdit=()=>{
+
+  //저장버튼(앨범 수정 닫기 기능으로 대체)
+  const tosave=()=>{
+    // const album = firestore.collection("album")
+    // album.doc(albumName+userEmail).set(
+    //   {
+    //     title:albumName,
+    //     creater:userEmail,
+    //     theme:selectedTheme,
+    //     image:selectedImages
+    //   })
     setShowEdit(false);
+    // album.doc(albumName+userEmail).get().then((document)=>{
+    //   const albumInfo = document.data();
+    //   })
   }
 
+  //포토북 페이지 
   const Pages=(theme, selectedImages)=>{
-     const result=[];
+     const result=[]; 
       for(let i=0; i< selectedImages.length; i++){
         result.push(<Page theme={theme[i%theme.length]}><Image src={selectedImages[i]}/></Page>)
       }
       return result;
   }
-  
+
   return (
     <Container>
         {showMenu?<Menu onclick={tocloseMenu}/>:""}
-        <Header starOnclick={toshowMenu} leftButton={showEdit?"save":"edit"} leftButtonOnclick={showEdit?tocloseEdit:toshowEdit} rightButton={showEdit?"delete":null}title={albumName}/>
-        {showEdit?<Edit/>:
+        <Header starOnclick={toshowMenu} leftButton={showEdit?"save":"edit"} leftButtonOnclick={showEdit?tosave:toshowEdit} rightButton={showEdit?"delete":null}title={albumName}/>
+        {showEdit?<Edit toshowMenu={toshowMenu} selectedPhoto={selectedImages} selectedTheme ={forPropsThemeName} albumName={albumName}/>:
+        <>
         <MainContainer>
                 <HTMLFlipBook
                     width={700}
                     height={900} 
                     size="stretch"
                     minWidth={300}
-                    // maxWidth={1500}
-                    // minHeight={1000}
-                    // maxHeight={1000}
                     maxShadowOpacity={0.3}
-                    showCover={true}
+                    showCover={false}
                     mobileScrollSupport={true}
-                    // onFlip={onPage}
                     className="demo-book">
                     {Pages(theme, selectedImages)}
-                        {/* <Page theme="url('./theme1.svg')"><Image src={frame}/></Page>
-                        <Page theme={theme1}><Image src={frame}/></Page>
-                        <Page theme={theme1}><Image src={frame}/></Page>
-                        <Page theme={theme1}><Image src={frame}/></Page> */}
                  </HTMLFlipBook>
-        </MainContainer>}
+        </MainContainer>
+        </>}
     </Container>
   );
 }
@@ -132,7 +130,6 @@ const MainContainer = styled.div`
     margin: auto;
     padding-top:130px;
     display:flex;
-    // border:1px solid black;
     text-align:center;
     @media Screen and (max-width:1000px){
         width:600px;
@@ -167,14 +164,19 @@ const Page = styled.div`
     }
 `
 const Image = styled.img`
-  width:400px;
+  width:160px;
   height:450px;
   margin-top:30px;
   object-fil:cover;
+  border:1px solid black;
   @media Screen and (max-width:1000px){
-      width:270px;
-      height:360px;
-      margin-top:10px;
+      width:120px;
+      height:320px;
+      margin-top:30px;
   }
+  @media Screen and (max-width:600px){
+    width:120px;
+    padding-top:0px;
+}
 `
 export default PhotoBook;
