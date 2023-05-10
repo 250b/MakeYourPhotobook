0687e2e12da80e965b2c3c1bcf8c09c2a99b3d0a
이보현 <84188904+250b@users.noreply.github.com>
2023-05-10 17:09:59 +0900

    [Main] 코드 정돈 및 주석 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,100 +4,72 @@ import React from "react";
 import Menu from "./Menu";
 import { useState,useRef,useCallback,useEffect } from "react";
 import Header from "../components/Header";
-import { onAuthStateChanged, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import {getStorage, ref, uploadBytes, listAll, getDownloadURL,} from "firebase/storage";
-import { firestore ,firebaseApp,
-  firebaseDb,
-  firebaseStorage,} from '../firebase';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import {getStorage, ref, uploadBytes, getDownloadURL,} from "firebase/storage";
+import { firestore ,firebaseApp} from '../firebase';
 
 function Main() {
-  // const [userEmail, setUserEmail] =  useState()
 
+  //유저 정보 가져오기
   const auth = getAuth();
   let userEmail=""
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    userEmail = user.email
-    // setUserEmail(user.email)
-    // ...
-  } else {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      userEmail = user.email
+    } else {
+    }
+  });
 
-  }
-});
 
-    const [showMenu, setShowMenu] = useState(false);
-
+  const [showMenu, setShowMenu] = useState(false);
+  //메뉴 열기
     const toshowMenu = ()=>{
         setShowMenu(true)
     }
+
+  //메뉴 닫기
     const tocloseMenu = ()=>{
       setShowMenu(false)
     }
-  
+ 
+  // 사진 입력 받기
     const photoInput = useRef();
+    const [imageUpload, setImageUpload] = useState("");
     
     const imgInput=()=>{
-      console.log("clicked");
       photoInput.current.click();
     }
-    
 
-
-
-    const [imageUpload, setImageUpload] = useState("");
-      const storage = getStorage(firebaseApp);
+    const storage = getStorage(firebaseApp);
 
     const onUploadImage = useCallback((e) => {
       if (e.target.files) {
-        console.log(e.target.files[0])
         setImageUpload(e.target.files[0]);
-        console.log(e.target.files[0])
         return;
       }
-      console.log(imageUpload);
     }, []);
 
-
+  //firebase, firestore에 이미지 정보 저장
     useEffect(() => {
       if (!imageUpload) return;
-      const imageRef = ref(storage, `image/${imageUpload.name}`);
-      uploadBytes(imageRef, imageUpload).then((snapshot) => {
-        getDownloadURL(snapshot.ref).then((url) => {
-          console.log(url)
+      const imageRef = ref(storage, `image/${imageUpload.name+userEmail}`) //파일 이름을 포함하여 파일의 전체 경로를 가리키는 참조
+      uploadBytes(imageRef, imageUpload).then((snapshot) => { //firestore 이미지 업로드
+        getDownloadURL(snapshot.ref).then((url) => { //firebase에 이미지 정보 저장
           const image = firestore.collection("image");
-        image.doc(imageUpload.name).set({
+        image.doc(imageUpload.name+userEmail).set({ //이미지 정보에 새 이미지 추가
             imgUrl: url,
-            creater:userEmail,
-            album:[]
+            creater:userEmail
           });
-
           const user = firestore.collection("user");
-          user.doc(userEmail).get().then((doc) => {
+          user.doc(userEmail).get().then((doc) => {//유저 정보에 새 이미지 추가
           const userInfo = doc.data();
-          const newList = [...userInfo.image, imageUpload.name];
-          console.log(newList);
+          const newList = [...userInfo.image, imageUpload.name+userEmail];
           user.doc(userEmail).update({image:newList});
-
           setImageUpload("");
+          alert("이미지가 성공적으로 업로드되었습니다.")
         });
       });
     }, [imageUpload])});
-
-
-    // const uploadImgUrl = async () => {
-    //   await addDoc(collection(firebaseDb, "image"), {
-    //     imgUrl: image,
-    //     creater: userEmail
-    //   });
-    //   setImageUpload("");
-    //   setImage("");
-    // };
-
-    
-    // // first time fetch
-    // useEffect(() => {
-    //   fetchImages();
-    // }, []);
 
   return (
     <Container>
@@ -109,7 +81,7 @@ onAuthStateChanged(auth, (user) => {
                 <Frame src={frame} onClick={imgInput}/>
                 <input type="file" accept="image/jpg, image/jpeg, image/png" ref={photoInput} onChange={onUploadImage} style={{display: "none"}}/>
                 <InputContainer>
-                    <Input placeholder="위 아이콘을 눌러 사진을 등록하거나 링크를 입력해주세요"/>
+                    <Input placeholder="위 아이콘을 눌러 사진을 등록해주세요"/>
                     <Button>GO!</Button>
                 </InputContainer>
                 <Title>PHOTOBOOK</Title>
