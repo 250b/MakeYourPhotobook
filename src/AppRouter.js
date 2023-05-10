39901782bfd18866804c18a1d96fc19cda86efb2
이보현 <84188904+250b@users.noreply.github.com>
2023-05-10 17:14:34 +0900

    로그인 후 서비스 이용 설정

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes, Router, Redirect, Navigate} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Start from './pages/Start';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import MyAlbum from './pages/MyAlbum';
 import PhotoBook from './pages/PhotoBook';
 import CreateAlbum from './pages/CreateAlbum';
-import { useEffect } from 'react';
-import {  getAuth,onAuthStateChanged } from 'firebase/auth';
 
 
 function AppRouter({isLoggedIn}) {
@@ -17,7 +15,13 @@ function AppRouter({isLoggedIn}) {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {isLoggedIn?
+          <Route path="/" element={<Start/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/main' element ={<Main/>}/>
+            <Route path='/myalbum' element ={<MyAlbum/>}/>
+            <Route path='/album' element ={<PhotoBook/>}/>
+            <Route path='/createAlbum' element ={<CreateAlbum/>}/>
+          {/* {isLoggedIn?
           <>
             <Route path="/" element={<Start/>}/>
             <Route path='/login' element={<Login/>}/>
@@ -31,7 +35,7 @@ function AppRouter({isLoggedIn}) {
             <Route path='/login' element={<Login/>}/>
             <Route path="*" element={<Navigate to ="/" />}/>
           </>
-        }
+        } */}
         </Routes>
       </BrowserRouter>
       
